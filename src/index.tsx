04057c3d35e2b0ad54ef939819a0c7b60c021be3
Breg,Coen C.J <c.breg@student.fontys.nl>
2021-08-31 19:14:53 +0200

    feat: working fist react app versino

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import App from './App';
+import React from 'react';
+import {  View } from 'react-native';
+
+const Root = (): JSX.Element => {
+    return (
+      <View>
+          <App />
+      </View>
+    );
+};
+
+export default Root;
