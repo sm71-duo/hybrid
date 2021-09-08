27f89c2796acba3d1f0c757a45558aeda09f1b28
Breg,Coen C.J <c.breg@student.fontys.nl>
2021-09-08 12:23:22 +0200

    feat: added testing file

---
+++ src/Views/AudioView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AudioView = () => {
+    return (
+        <View>
+            <Text>yeet</Text>
+        </View>
+    );
+};
+
+export default AudioView;
