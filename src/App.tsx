78b0b5d9eb1e06728c4b7d6b65095abf2cc8e077
Breg,Coen C.J <c.breg@student.fontys.nl>
2021-09-15 09:49:45 +0200

    feat: init socket io start

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import LiveAudioStreamView from 'Views/LiveAudioStreamView'; //Could be usefull
 import RecordingView from 'Views/RecordingView'; //Not usefull
+import SocketIoView from 'Views/SocketIoView';
 
 const App = () => {
     return (
---
+++ src/Views/SocketIoView.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const socketIoView = () => {
+    return (
+        <Wrapper>
+            <Text>oi</Text>
+        </Wrapper>
+    );
+};
+//https://tutorialedge.net/typescript/typescript-socket-io-tutorial/
+const Wrapper = styled.View`
+    margin: 24px;
+`;
+
+export default socketIoView;
