4046437390793c843ac3b84bf831bc501b38c17b
Breg,Coen C.J <c.breg@student.fontys.nl>
2021-09-15 13:44:22 +0200

    feat: added basic tab navigation

---
+++ src/views/HomeOverview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeOverview = () => {
+    return (
+        <View>
+            <Text>HomeOverview</Text>
+        </View>
+    );
+};
+
+export default HomeOverview;
