6bce5a86e0a4d2fd7df9cf5562db59069639cd65
Roy <roy.appeldoorn@gmail.com>
2021-09-15 13:25:40 +0200

    removed unnused button

--- src/views/Details.tsx
+++ src/views/Details.tsx
@@ -9,7 +9,6 @@ const Details = ({ navigation }: Props) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Button title="Go to Details... again" onPress={() => navigation.push('Details')} />
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back to first screen in stack" onPress={() => navigation.popToTop()} />
     </View>
